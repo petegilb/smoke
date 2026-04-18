5fca3f74f3cd2317f3fcafeb498852ee69cd36a4
Peter Gilbert <petergilbert360@gmail.com>
2026-04-18 22:31:08 +0900

    get steam search results in json format and parse appid from logo url

--- backend/main.go
+++ backend/main.go
@@ -2,28 +2,58 @@ package main
 
 import (
     "fmt"
+    "log"
     "net/http"
     "os"
     "io"
+    "encoding/json"
+    "regexp"
+    "strconv"
 )
 
-func main() {
-    fmt.Println("Hello, World!")
+var appIDRegex = regexp.MustCompile(`/apps/(\d+)/`)
 
-    requestURL := fmt.Sprintf("https://www.valvesoftware.com/about/stats")
+type storeResponse struct {
+    Desc string `json:"desc"`
+	Items []struct {
+		Name string `json:"name"`
+		Logo string `json:"logo"`
+	} `json:"items"`
+}
+
+
+func main() {
+    log.Println("Starting Steam store search...")
+
+    requestURL := fmt.Sprintf("https://store.steampowered.com/search/results/?filter=popularwishlist&json=1&count=500&page=1")
     res, err := http.Get(requestURL)
     if err != nil {
-        fmt.Printf("error making http request: %s\n", err)
+        log.Printf("error making http request: %s\n", err)
         os.Exit(1)
     }
 
     resBody, err := io.ReadAll(res.Body)
     if err != nil {
-        fmt.Printf("client: could not read response body: %s\n", err)
+        log.Printf("client: could not read response body: %s\n", err)
         os.Exit(1)
     }
-    fmt.Printf("client: response body: %s\n", resBody)
 
-    fmt.Printf("client: got response!\n")
-    fmt.Printf("client: status code: %d\n", res.StatusCode)
+    log.Printf("client: got response!\n")
+    log.Printf("client: status code: %d\n", res.StatusCode)
+
+    var payload storeResponse
+    err = json.Unmarshal(resBody, &payload)
+    if err != nil {
+        log.Fatal("Error during Unmarshal(): ", err)
+        os.Exit(1)
+    }
+
+    for i, game := range payload.Items {
+        // Check if we can grab the appid from the url using regex
+        m := appIDRegex.FindStringSubmatch(game.Logo)
+        if len(m) >= 2 {
+            appID, _ := strconv.Atoi(m[1])
+            log.Printf("#%d: %s (appid: %d)\n", i+1, game.Name, appID)
+        }
+    }
 }
