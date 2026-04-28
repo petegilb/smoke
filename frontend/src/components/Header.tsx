dca7bc7fa5eec877df8a2d6a967b65be97c867d2
Peter Gilbert <petergilbert360@gmail.com>
2026-04-28 17:53:40 +0900

    set daisyui theme

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,13 +2,17 @@ import { Link } from 'react-router'
 
 function Header() {
   return (
-    <header>
-      <Link to="/">smoke</Link>
-      <nav>
-        <Link to="/rankings">Rankings</Link>
-        <Link to="/games">Games</Link>
-      </nav>
-    </header>
+    <div className="navbar bg-base-200">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl">smoke</Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li><Link to="/rankings">Rankings</Link></li>
+          <li><Link to="/games">Games</Link></li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
